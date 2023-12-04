c80abdd34cf59fdef276226808c75912993724a5
Andre Gonçalves <dregoncalves22@gmail.com>
2023-12-03 21:34:34 -0300

    Melhorias na eficiência da busca de arquivos

--- src/br/com/unicuritiba/Main.java
+++ src/br/com/unicuritiba/Main.java
@@ -11,17 +11,12 @@ public class Main {
         String diretorioInicial = "C://";
 
         // Os arquivos procurados
-        String alvo = "execucao-aula-teste.exe";
-        String alvo2 = "execucao-aula.exe";
-        String alvo3 = "script-aula-teste.bat";
-        String alvo4 = "script-aula.bat";
+        String[] alvos = {"execucao-aula-teste.exe", "execucao-aula.exe", "script-aula-teste.bat", "script-aula.bat"};;
 
         // Instanciando a classe BuscadorDeArquivos
         BuscadorDeArquivos buscador = new BuscadorDeArquivos();
 
         // Chama o método de busca
-        buscador.procurarArquivos(new File(diretorioInicial), alvo, alvo2, alvo3, alvo4);
-        // como ele ta fazendo um a um a busca ta meio demorado, da pra alterar o metodo colocando
-        // 4 entradas e verificando se é alguma delas
+        buscador.procurarArquivos(new File(diretorioInicial), alvos);
     }
 }
--- src/br/com/unicuritiba/utilities/BuscadorDeArquivos.java
+++ src/br/com/unicuritiba/utilities/BuscadorDeArquivos.java
@@ -3,7 +3,7 @@ package br.com.unicuritiba.utilities;
 import java.io.File;
 
 public class BuscadorDeArquivos {
-      public void procurarArquivos(File diretorio, String alvo, String alvo2, String alvo3, String alvo4) {
+      public void procurarArquivos(File diretorio, String[] alvos) {
         // Procura os arquivos e pastas na pasta atual
         File[] arquivos = diretorio.listFiles();
 
@@ -11,12 +11,16 @@ public class BuscadorDeArquivos {
             for (File arquivo : arquivos) {
                 if (arquivo.isDirectory()) {
                     // Se for uma pasta, chama de novo o método para dentro dela
-                    procurarArquivos(arquivo, alvo, alvo2, alvo3, alvo4);
+                    procurarArquivos(arquivo, alvos);
                 } else {
                     // Se for um arquivo, verifica se o nome está correto
-                    if (arquivo.getName().endsWith(alvo)|| arquivo.getName().endsWith(alvo2)||arquivo.getName().endsWith(alvo3)||arquivo.getName().endsWith(alvo4)) {
-                        System.out.println("Nome do arquivo: " + arquivo.getName());
-                        System.out.println("Arquivo encontrado em: " + arquivo.getAbsolutePath());
+                    for (String alvo : alvos){
+                        if (arquivo.getName().endsWith(alvo)) {
+                            System.out.println("---------------------------------------------------");
+                            System.out.println("Nome do arquivo: " + arquivo.getName());
+                            System.out.println("Arquivo encontrado em: " + arquivo.getAbsolutePath());
+                            break;
+                        }
                     }
                 }
             }
