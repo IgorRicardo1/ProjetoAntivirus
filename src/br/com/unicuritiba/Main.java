2b6cbac55014a30b26c107d5007285d90ae66293
Igor Ricardo <122498648+IgorRicardo1@users.noreply.github.com>
2023-12-05 23:31:39 -0300

    adição das primeiras interações com a tela

--- src/br/com/unicuritiba/Main.java
+++ src/br/com/unicuritiba/Main.java
@@ -1,22 +1,69 @@
 package br.com.unicuritiba;
-
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
 
 import br.com.unicuritiba.utilities.BuscadorDeArquivos;
 
 public class Main {
 
     public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            interfaceGrafica();
+        });
+    }
+
+    private static void interfaceGrafica() {
+        JFrame frame = new JFrame("Antivirus DiegoHunter");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+        frame.setLayout(new GridLayout(5, 1));
+
+        JCheckBox checkBox1 = new JCheckBox("execucao-aula-teste.exe");
+        JCheckBox checkBox2 = new JCheckBox("execucao-aula.exe");
+        JCheckBox checkBox3 = new JCheckBox("script-aula-teste.bat");
+        JCheckBox checkBox4 = new JCheckBox("script-aula.bat");
+
+        JButton closeButton = new JButton("Fechar");
+        JButton eventButton = new JButton("Iniciar Procura");
+
         // Onde começa a busca
         String diretorioInicial = "C://";
 
         // Os arquivos procurados
-        String[] alvos = {"execucao-aula-teste.exe", "execucao-aula.exe", "script-aula-teste.bat", "script-aula.bat"};;
+        String[] alvos = {"execucao-aula-teste.exe", "execucao-aula.exe", "script-aula-teste.bat", "script-aula.bat"};
 
         // Instanciando a classe BuscadorDeArquivos
         BuscadorDeArquivos buscador = new BuscadorDeArquivos();
 
-        // Chama o método de busca
-        buscador.procurarArquivos(new File(diretorioInicial), alvos);
+        eventButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+                JOptionPane.showMessageDialog(frame, buscador.procurarArquivos(new File(diretorioInicial), alvos));
+            }
+        });
+
+        closeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+        frame.add(checkBox1);
+        frame.add(checkBox2);
+        frame.add(checkBox3);
+        frame.add(checkBox4);
+        frame.add(closeButton);
+        frame.add(eventButton);
+
+        frame.setVisible(true);
     }
 }
--- src/br/com/unicuritiba/utilities/BuscadorDeArquivos.java
+++ src/br/com/unicuritiba/utilities/BuscadorDeArquivos.java
@@ -3,7 +3,9 @@ package br.com.unicuritiba.utilities;
 import java.io.File;
 
 public class BuscadorDeArquivos {
-      public void procurarArquivos(File diretorio, String[] alvos) {
+	public String procurarArquivos(File diretorio, String[] alvos) {
+        StringBuilder resultado = new StringBuilder();
+
         // Procura os arquivos e pastas na pasta atual
         File[] arquivos = diretorio.listFiles();
 
@@ -11,19 +13,25 @@ public class BuscadorDeArquivos {
             for (File arquivo : arquivos) {
                 if (arquivo.isDirectory()) {
                     // Se for uma pasta, chama de novo o método para dentro dela
-                    procurarArquivos(arquivo, alvos);
+                    resultado.append(procurarArquivos(arquivo, alvos));
                 } else {
                     // Se for um arquivo, verifica se o nome está correto
-                    for (String alvo : alvos){
+                    for (String alvo : alvos) {
                         if (arquivo.getName().endsWith(alvo)) {
-                            System.out.println("---------------------------------------------------");
-                            System.out.println("Nome do arquivo: " + arquivo.getName());
-                            System.out.println("Arquivo encontrado em: " + arquivo.getAbsolutePath());
+                            resultado.append("---------------------------------------------------\n");
+                            resultado.append("Nome do arquivo: ").append(arquivo.getName()).append("\n");
+                            resultado.append("Arquivo encontrado em: ").append(arquivo.getAbsolutePath()).append("\n");
                             break;
                         }
                     }
                 }
             }
+        } else {
+            System.out.println("Nenhum arquivo encontrado no diretório: " + diretorio.getAbsolutePath());
+           
         }
+
+        return resultado.toString();
     }
 }
+
